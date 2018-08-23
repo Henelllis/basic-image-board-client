05b96a16184a517cbbb0db805902ea9dd91db058
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-08-23 09:51:37 -0400

    Design of Post Form

--- src/containers/PostForm/PostForm.js
+++ src/containers/PostForm/PostForm.js
@@ -3,7 +3,7 @@ import '../../App.css';
 
 class PostForm extends Component {
     state = {
-        isFormShown: true
+        isFormShown: false
     }
 
     onThreadStartClick = () => {
@@ -12,43 +12,50 @@ class PostForm extends Component {
 
     render() {
 
-//HTML FOR FORM, GET INTO JSX
-{/* <div class='Flex'>
-<form class='TestGrid' action="">
-  <label class='Test' for="x">Name</label>
-  <input class='text' type="text">
-  <div></div>
-    <label class='Test' for="x">Options</label>
-  <input class='text' type="text">
-  <div></div>
-  <label class='Test' for="x">Subject</label>
-  <input class='text' type="text">
-  <button class='btn'>POST</button>
-   <label class='Bigbox' for="x">Comment</label>
-      <textArea class='long' type="text"></textArea>
-   <label class='Bigbox' for="x">Verification</label>
-    <div class="captcha-box">
-          <input class='captcha' type="text" disabled placeholder='INTEGRATE CAPTCHA LATER'>
-      <p class="captcha-text"> Get a special pass to bypass this</p>
-    </div>
-    <label class='Test' for="x">File</label>
-  <input type="file"/>
-  <div class=""> [ <input type="radio"/> Spoiler ?] </div>
- </form>
-</div> */}
+
         let postForm = null;
 
         if(this.state.isFormShown){
             postForm = (
-                <form className='Post__form--form'>
-                    <div className="Post__form--form-label-group">
-                        <label className='Post__form--form-label' for='UserName'>Name</label>
-                        <input name='UserName' type="text"/>
-                    </div>
-                    <div className="Post__form--form-label-group">
-                        <input type="text"/>
-                    </div>
-                </form>
+                <div className="Post__form__expanded--container">
+                    <form action="" className="Post__form__expanded--form">
+                        {/* Name field */}
+                        <label htmlFor="nameField" className="Post__form__expanded--label">Name</label>
+                        <input name="nameField" 
+                                type="text" 
+                                className="Post__form__expanded--text"
+                                placeholder="Anonymous"/>
+                        <div></div>
+                        {/*Options field*/}
+                        <label htmlFor="optionField" className="Post__form__expanded--label">Options</label>
+                        <input name="optionField" type="text" className="Post__form__expanded--text"/>
+                        <div></div>
+                        {/*Subject field And Post Button*/}
+                        <label htmlFor="SubjectField" className="Post__form__expanded--label">Subject</label>
+                        <input name="SubjectField" type="text" className="Post__form__expanded--text"/>
+                        <button class='Post__form__expanded--btn'>POST</button>
+                        {/* Comment field */}
+                        <label htmlFor="CommentField" className="Post__form__expanded--label">Comment</label>
+                        <textArea class='Post__form__expanded--area' type="text"></textArea>
+                        {/* Verification Field */}
+                        <label htmlFor="VerificationField" className="Post__form__expanded--label">Verification</label>
+                        <div class='Post__form__expanded--captcha-container'>
+                            <input name="VerificationField" 
+                                   type="text" 
+                                   className="Post__form__expanded--captcha-text"
+                                   disabled placeholder='INTEGRATE CAPTCHA LATER'/>
+                                   <p class="Post__form__expanded--captcha-pass"> Get a special pass to bypass this</p>
+                        </div>
+                        {/* FILE field */}
+                        <label htmlFor="FileField" className="Post__form__expanded--label">File</label>
+                        <input type="file"/>
+                        <div className="Post__form__expanded--radio"> [<input type="radio"/> Spoiler?]</div>
+                        <p class="Post__form__expanded--captcha-rules"> 
+                            &bull; Please read the <a  className="Post__form__expanded--anchor" href="/" >Rules </a> 
+                                    and <a  className="Post__form__expanded--anchor" href="/" >FAQ </a> before posting.
+                        </p>
+                    </form>
+                </div>
             )
         }else {
             postForm = (
